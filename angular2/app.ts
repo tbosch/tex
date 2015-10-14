8483c74959e006630984c925a1180aa0667277f0
vsavkin <avix1000@gmail.com>
2015-10-14 11:15:08 -0700

    add more data

--- angular2/app.ts
+++ angular2/app.ts
@@ -11,6 +11,7 @@ var q = n;
       Speaker: <input ng-control="speaker" minlength="3">
       Title: <input ng-control="title" minlength="3">
 	  </form>
+
     <ul>
       <template ng-for [ng-for-of]="talks" [ng-for-template]="itemTmpl"/>
     </ul>
@@ -65,7 +66,11 @@ class App {
 
     {id: 2, title: 'Testing strategies with Angular 2', speaker: 'Julie Ralph', description: 'Angular loves testability, and Angular 2 will continue to make it easy to write great test suites so that you’re confident in your site. Learn how to use karma and other tools to set up and debug tests, see how the Angular team creates their test suite, and meet new test helpers just for Angular 2 components.'},
 
-    {id: 3, title: 'Building the Best Components', speaker: 'Jeremy Elbourn', description: 'The component is the new atomic unit of an Angular 2 application. So what makes a good component? This talk will explore how Angular 2 components are different from the directives you’re used to and provide some practical guidance on building them. We’ll also look at different types of tests you can write to guard against all kinds of regressions.'}
+    {id: 3, title: 'Building the Best Components', speaker: 'Jeremy Elbourn', description: 'The component is the new atomic unit of an Angular 2 application. So what makes a good component? This talk will explore how Angular 2 components are different from the directives you’re used to and provide some practical guidance on building them. We’ll also look at different types of tests you can write to guard against all kinds of regressions.'},
+
+		{id: 4, title: 'Creating realtime apps with Angular 2 and Meteor', speaker: 'Uri Goldshtein', description: 'Meteor is an open source JavaScript application platform for building realtime web and mobile applications. Meteor is also the perfect backend for Angular 2.0 applications. In this talk, you will learn how to create a real time full-stack Angular 2.0 Meteor app in minutes. You will also learn about the details of the integration, the similarities between the philosophies of both platforms, and the future collaborations between the two.'},
+
+		{id: 5, title: 'Using Web Workers for more responsive apps', speaker: 'Jason Teplitz', description: 'Angular 2’s split rendering structure allow us to automagically run most of Angular and your code on a separate processor core via Web Workers. See live demos, and find out how to get started on the early version of this feature.'}
   ];
 }
 bootstrap(App);
